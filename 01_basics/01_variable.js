fe27f5a6c06d3d745b207d9b6a23f47685356999
ekta050603-coder <ekta050603@gmail.com>
2025-12-25 16:55:40 +0000

    adding variables and datatypes files

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,7 @@
+const AccountId=12345
+let Accountemail="abc@gmail.com"
+var AccountPassword="1234"
+Accountemail="123@gmail.com"
+Bankcountry="jaipur"
+Bankname;
+console.table([AccountId,Accountemail,AccountPassword,Bankcountry,Bankname])
---
+++ 01_basics/02-datatypes.js
@@ -0,0 +1,19 @@
+"use strict"//trat all the js code as a newer version 
+//alert(3+3)//we are node js, not browser
+console.log(3+3)//code readability
+console.log("Hitesh")
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+//number=>2 to power 53
+//bigInt
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+
+console.log(typeof undefined);// undefined
+//console.log(typeOf null); // object
